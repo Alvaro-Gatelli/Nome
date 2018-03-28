588b78924e6d57b4d9e2e7333e5dc85b7a8f1e4d
Alvaro Gatelli <37388596+Alvaro-Gatelli@users.noreply.github.com>
2018-03-28 01:26:04 -0300

    Update Conversão Cambial.py

--- Conversão Cambial.py
+++ Conversão Cambial.py
@@ -1,21 +1,18 @@
 print('~'*20+'Conversão de dolar'+15*'~')
 
-#│=Dinheiro para ser transformado em dolar ou vice-versa
+#│=Dinheiro para ser transformado de real para dolar
 #↓
 dinheiro=float(input("Digite quanto dinheiro você quer converter em doalres: "))
 
-#~~~~~│=Dinheiro em reais vai para dinheiro em dolar
-#     ↓
 print('De:',dinheiro,'R$ Para:',dinheiro/100*33,'$')
 
-#│pergunta se você quer transformar seus dolares em reais novamente
+#│=Dinheiro para ser transformado de dolar para real
 #↓
-pergunta=input('Quer re-converter o dolar para real?s/n: ')
-
-#│=Se a resposta da pergunta for sim então o programa vai re-converter de dolar para real
-#↓
+pergunta=input('Você Deseja converter de dolares para reais?s/n: ')
 if pergunta == 's':
-    print('De:',dinheiro/100*33,'$ Para:',dinheiro)
+    dinheiro2=float(input('Digite uma quantia em dolares que você queira converter para reais: '))
+    print('De:',dinheiro2,'$ Para:',dinheiro2/100*330)
 else:
-    print('Seu dinheiro é de:',dinheiro/100*33)
-print('~'*53)
+    print('Seu saldo é de:',dinheiro)
+
+print('~'*20+'Conversão de dolar'+15*'~')

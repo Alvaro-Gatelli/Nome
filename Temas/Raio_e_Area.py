f3c1a4263f6e4e0e087d02c4f678ce52980afd1a
Alvaro Gatelli <37388596+Alvaro-Gatelli@users.noreply.github.com>
2018-03-28 02:34:28 -0300

    Add files via upload
    Medida de raio e area

---
+++ Temas/Raio_e_Area.py
@@ -0,0 +1,7 @@
+print('~'*12+'Raio e Area'+12*'~')
+a=float(input('Qual a altura do seu retangulo?: '))
+b=float(input("Qual é o tamanho da base do seu retangulo?: "))
+
+print("O raio do rentagulo é de:",2*(a+b),'cm')
+print("E a area do retangulo é de:",a*b,'cm Quadrados')
+print('~'*35)
